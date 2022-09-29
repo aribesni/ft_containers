816c1cd765946f624bbf9b40e34d32f56154ce22
Aristide BESNIER <aribesni@e1r6p10.clusters.42paris.fr>
2022-09-29 16:36:36 +0200

    set on the way

--- project/includes/set.hpp
+++ project/includes/set.hpp
@@ -41,7 +41,60 @@ namespace ft {
                 typedef ft::SetReverseIterator<iterator>        reverse_iterator;
                 typedef ft::SetReverseIterator<const_iterator>  const_reverse_iterator;
 
+            //MEMBER FUNCTIONS
 
+                //Constructors
+                explicit set(const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) {}
+                template <class InputIterator>
+                    set(InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) {}
+                set(const set& x) {}
+
+                //Destructor
+                ~set(void) {}
+
+                //Operators
+                set&    operator=(const set& x) {}
+
+                //Iterators
+                iterator                begin(void) {}
+                const_iterator          begin(void) const {}
+                iterator                end(void) {}
+                const_iterator          end(void) const {}
+                reverse_iterator        rbegin(void) {}
+                const_reverse_iterator  rbegin(void) const {}
+                reverse_iterator        rend(void) {}
+                const_reverse_iterator  rend(void) const {}
+
+                //Capacity
+                bool        empty(void) const {}
+                size_type   size(void) const {}
+                size_type   max_size(void) const {}
+
+                //Modifiers
+                pair<iterator, bool>
+                    insert(const value_type& val) {}
+                iterator    insert(iterator position, const value_type& val) {}
+                template <class InputIterator>
+                    void    insert(InputIterator first, InputIterator last) {}
+                void        erase(iterator position) {}
+                size_type   erase(const value_type& val) {}
+                void        erase(iterator first, iterator last) {}
+                void        swap(set& x) {}
+                void        clear(void) {}
+
+                //Observers
+                key_compare     key_comp(void) const {}
+                value_compare   value_comp(void) const {}
+
+                //Operations
+                iterator                    find(const value_type& val) const {}
+                size_type                   count(const value_type& val) const {}
+                iterator                    lower_bound(const value_type& val) const {}
+                iterator                    upper_bound(const value_type& val) const {}
+                pair<iterator, iterator>    equal_range(const value_type& val) const {}
+
+                //Allocator
+                allocator_type  get_allocator(void) const {}
         };
 }
 
